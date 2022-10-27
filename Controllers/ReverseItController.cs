dab6d531ee7e0fd2c883116750663a808ddce589
Reed Goodwin <113049336+Soup-Mann@users.noreply.github.com>
2022-10-27 15:01:49 -0700

    Fixed

--- Controllers/ReverseItController.cs
+++ Controllers/ReverseItController.cs
@@ -1,3 +1,11 @@
+//Reed Goodwin
+
+//10-27-2022
+
+//Peer reviewed Dan Decoiton: The program and api work and run well. I like the fact that you didnt have a route name. I didnt realize you could do that. Everything is working great just dont forget to add a gitignore.
+
+//Reverse it- Will reverse a number then a letter
+
 using Microsoft.AspNetCore.Mvc;
 
 namespace GoodwinR_ReverseItEndPoint.Controllers;
