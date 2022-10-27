4dc355042237e38a88f46fd7d7f5525db73e0dad
Reed Goodwin <113049336+Soup-Mann@users.noreply.github.com>
2022-10-27 09:58:16 -0700

    ReverseEndpoint

---
+++ Controllers/ReverseItController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoodwinR_ReverseItEndPoint.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ReverseController : ControllerBase
+{
+[HttpGet]
+[Route("{num1}/{word}")]
+public string rev(string num1,string word){
+bool isNumber;
+bool isWord;
+long validNum = 0;
+isNumber = Int64.TryParse(num1, out validNum);
+isWord = Int64.TryParse(word, out validNum);
+
+if(isNumber == true && isWord == false){
+   string fuck = String.Empty;
+            for (int i = num1.Length - 1; i > -1; i--)
+            {
+               fuck += num1[i];
+               
+            };
+            var resultstring = new string(word.ToCharArray().Reverse().ToArray());
+return $"{num1} reversed is {fuck}\n{word} reversed is {resultstring}!!";
+}else{
+    return "Please enter a valid Number and Word (in that order!)";
+}
+}
+
+
+
+}
